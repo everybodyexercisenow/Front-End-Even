c9f342e63af2f33d9c62954b37bcb6c16a269377
Vivian Zhu <tongyu@usc.edu>
2019-02-16 12:58:20 -0800

    fix homepage

--- screens/Demo.js
+++ screens/Demo.js
@@ -6,36 +6,37 @@ import Icon from 'react-native-vector-icons/Ionicons';
 export default class Demo extends Component {
   constructor(props) {
     super(props);
+    console.log("Constructor")
     this.state = {
         selectedIndex:0
     };
   }
-  switchScreen(){
-    if(this.state.selectedIndex == 0){
+  switchScreen(index){
+    if(index == 1){
       this.props.navigation.navigate('Camera');
     }
   }
   render() {
-    return (
-        <SafeAreaView style={{flex:1, alignItems:'center'}}>
-            <SegmentedControlIOS
-            values={['Demo', 'Camera']}
-            selectedIndex={this.state.selectedIndex}
-            onChange={(event) => {
-                this.switchScreen();
-                this.setState({selectedIndex: event.nativeEvent.selectedSegmentIndex});
-            }}
-            tintColor='#83ABAA'
-            style={styles.segBar}
-            />
-            <View style={{flex:1, justifyContent:'flex-end',  alignItems: 'center', marginTop:10}}>
-                <Icon 
-                    name="ios-close-circle" 
-                    style={{fontSize:50, color:'tomato'}}
-                    onPress={()=> this.props.navigation.navigate('Home')}
-                />
-            </View>
-        </SafeAreaView>
+    return ( null
+        // <SafeAreaView style={{flex:1, alignItems:'center'}}>
+        //     <SegmentedControlIOS
+        //     values={['Demo', 'Camera']}
+        //     selectedIndex={this.state.selectedIndex}
+        //     onChange={(event) => {
+        //         this.switchScreen(event.nativeEvent.selectedSegmentIndex);
+        //         // this.setState({selectedIndex: event.nativeEvent.selectedSegmentIndex});
+        //     }}
+        //     tintColor='#83ABAA'
+        //     style={styles.segBar}
+        //     />
+            // <View style={{flex:1, justifyContent:'flex-end',  alignItems: 'center', marginTop:10}}>
+            //     <Icon 
+            //         name="ios-close-circle" 
+            //         style={{fontSize:50, color:'tomato'}}
+            //         onPress={()=> this.props.navigation.navigate('HomeScreen')}
+            //     />
+            // </View>
+        // </SafeAreaView>
     );
   }
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,27 +1,49 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text> Homepage </Text>
+        <View style={{ flex: 1, justifyContent: "flex-end", marginBottom: 30 }}>
+          <View style={{ height: 130, marginTop: 20 }}>
+            <ScrollView
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+            >
+              <Category
+                categoryName="Daily"
+                imageUri={require("../assets/1.jpg")}
+                name="Home"
+              />
+              <Category
+                categoryName="Monthly"
+                imageUri={require("../assets/2.jpg")}
+                name="Experiences"
+              />
+              <Category
+                categoryName="Yearly"
+                imageUri={require("../assets/3.jpg")}
+                name="Resturant"
+              />
+            </ScrollView>
+          </View>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
